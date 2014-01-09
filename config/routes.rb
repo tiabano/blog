db5331014f61c8a784dfc8d97e1dde6be27b248c
tiabano <tiabano04@gmail.com>
2014-01-08 23:07:01 -0500

    creating home and about us pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Blog::Application.routes.draw do
+  # get "pages/home"
+  root 'pages#home'
+  get 'about' => 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
